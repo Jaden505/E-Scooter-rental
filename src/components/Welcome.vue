2ae7ddfbb6a4354b41d6dcc8c77186c13f0c83c1
Jaden505 <jadenxxxl@gmail.com>
2022-09-12 20:24:11 +0200

    Complete left side of welcome page. Add brand choosing and availability

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>Welcome component</h1>
+  <div id="left-side">
+    <label for="standard-select" >Select a brand</label>
+    <div class="select">
+      <select id="standard-select">
+        <option value="ecooter">Ecooter</option>
+        <option value="niu">NIU</option>
+        <option value="agm">AGM</option>
+        <option value="kymco">Kymco</option>
+        <option value="btc">BTC</option>
+      </select>
+    </div>
+
+    <h1>Availability</h1>
+    <p>Leidseplein: <b>12</b></p>
+    <p>Rembrandtplein: <b>23</b></p>
+    <p>Centraal station: <b>8</b></p>
+  </div>
+
+  <div id="middle"></div>
+  <div id="right-side"></div>
 </template>
 
 <script>
@@ -8,20 +27,23 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  #left-side {
+    float: left;
+    padding-left: 2%;
+    padding-top: 2%;
+    width: 20%;
+  }
+
+  .select {
+    margin-bottom: 70px;
+  }
+
+  label {
+    font-size: 22px;
+  }
+
+  p {
+    font-size: 20px;
+  }
 </style>
