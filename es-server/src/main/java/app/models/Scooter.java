c42973520d13ac2ac6bdd18bf623bfde72c3d0d9
Joël Amarh <Joel.amarh@hva.nl>
2022-10-23 04:21:50 +0200

    @getmapping /scooter and Scooter model

--- es-server/src/main/java/app/models/Scooter.java
+++ es-server/src/main/java/app/models/Scooter.java
@@ -1,4 +1,34 @@
 package app.models;
 
 public class Scooter {
+
+    private long id;
+    private String tag;
+    private String status;
+    private String gpsLocation;
+    private int batteryCharge;
+    private int mileage;
+
+    public Scooter(long id, String tag, String status, String gpsLocation, int batteryCharge, int mileage) {
+        this.id = id;
+        this.tag = tag;
+        this.status = status;
+        this.gpsLocation = gpsLocation;
+        this.mileage = mileage;
+        this.batteryCharge = batteryCharge;
+    }
+
+
+
+    public Scooter(long id) {
+    }
+
+//    public Scooter createRepository(long id){
+//        Scooter scooter = new Scooter(id);
+//
+//    }
+
+
+
+
 }
--- es-server/src/main/java/app/rest/ScooterController.java
+++ es-server/src/main/java/app/rest/ScooterController.java
@@ -1,4 +1,18 @@
 package app.rest;
 
+import app.models.Scooter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
 public class ScooterController {
+    @GetMapping("/scooter")
+    public List<Scooter> getTestScooter (){
+        return List.of(
+                new Scooter(0,"Test-scooter-B","null","0",0,0));
+    }
+
+
 }
